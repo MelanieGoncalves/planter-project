8c9d2db41cd9fd52a3137b7872ef912275791063
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-09-22 10:01:49 -0700

    styles for side bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import GetData from './components/GetData';
+import SideBar from './components/SideBar';
 import TopBar from './components/TopBar';
+import styles from './styles/appstyles.module.css'
 
 function App() {
 
   return (
     <div className="App">
       <TopBar />
+      <div id={styles.sideBar}>
+        <SideBar />
+      </div>
       <GetData />
     </div>
   );
---
+++ src/components/SideBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+
+function SideBar() {
+    return (
+        <div>
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                </ListItem>
+            </List>
+
+        </div>
+    )
+}
+
+export default SideBar
