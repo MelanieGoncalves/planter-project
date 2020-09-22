dc102bb03cfce03e0812979e8ef1b33c45ac08e7
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-09-22 09:28:54 -0700

    Switched to Material-UI, added new top menu bar

---
+++ src/components/GetData.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Axios from 'axios';
+
+export class GetData extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        Axios.get('/api/v1/plants?token=' + process.env.REACT_APP_API_KEY)
+            .then(response => {
+                console.log(response)
+            }).catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default GetData
---
+++ src/components/TopBar.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import {
+    AppBar, Toolbar, Typography, IconButton,
+    MenuItem, Switch, FormControlLabel,
+    FormGroup, Menu, requirePropFactory
+} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function MenuAppBar() {
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.root}>
+            {/* <FormGroup>
+                <FormControlLabel
+                    control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+                    label={auth ? 'Logout' : 'Login'}
+                />
+            </FormGroup> */}
+
+            <AppBar position="static" color="inherit">
+                <Toolbar>
+                    <img alt="icon" src={require('../images/planter-icon.png')} style={{ width: "50px", height: "50px" }} />
+                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" className={classes.title}>
+                        THE PLANTER BOX
+          </Typography>
+                    {auth && (
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
